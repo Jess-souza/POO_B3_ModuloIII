ac728a43acb38117a2e5b8303d200ec440deb7c2
Jessica <jsouzasouza120@gmail.com>
2022-11-07 21:50:16 -0300

    Ajustes da classe Internacoes

--- src/br/com/ada/petshop/businessobject/Internacoes.java
+++ src/br/com/ada/petshop/businessobject/Internacoes.java
@@ -1,20 +1,24 @@
 package br.com.ada.petshop.businessobject;
+
 import br.com.ada.petshop.modelo.Animal;
+
 import java.util.Objects;
 
 public class Internacoes {
-    private Animal[] animalInternados = new Animal[10];
+    private Animal[] animaisInternados = new Animal[10];
 
     public void internar(Animal animal) {
-        for (int i = 0; i < animalInternados.length; i++) {
-            animalInternados[i] = animal;
-            break;
+        for (int i = 0; i < animaisInternados.length; i++) {
+            if (Objects.isNull(animaisInternados[i])) {
+                animaisInternados[i] = animal;
+                break;
+            }
         }
     }
 
     public void mostraAnimaisInternados() {
         System.out.println("-------------------------ANIMAIS INTERNADOS-------------------------");
-        for (Animal animal : animalInternados) {
+        for (Animal animal : animaisInternados) {
             if (Objects.nonNull(animal)) {
                 System.out.println(animal);
             }
