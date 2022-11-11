27867afa60fcc0e8da2526c535c50b86c961bf1a
Jessica <jsouzasouza120@gmail.com>
2022-11-11 19:14:07 -0300

    Preparação do conteúdo para exercicio de Interfaces

--- src/br/com/ada/petshop/modelo/Dono.java
+++ src/br/com/ada/petshop/modelo/Dono.java
@@ -1,65 +1,9 @@
 package br.com.ada.petshop.modelo;
 
-public class Dono {
-    private int id;
-    private String nome;
-    private String cpf;
-    private String email;
-    private Endereco endereco;
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getCpf() {
-        return cpf;
-    }
-
-    public void setCpf(String cpf) {
-        this.cpf = cpf;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public Endereco getEndereco() {
-        return endereco;
-    }
-
-    public void setEndereco(Endereco endereco) {
-        this.endereco = endereco;
-    }
+import java.util.Objects;
+public class Dono extends Pessoa{
 
     public Dono(String nome, Endereco endereco) {
-        this.nome = nome;
-        this.endereco = endereco;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        if (this.id == 0) {
-            this.id = id;
-        }
-    }
-
-    @Override
-    public String toString() {
-        return "Dono{" +
-                "id=" + id +
-                ", nome='" + nome + '\'' +
-                ", endereco=" + endereco +
-                '}';
+        super(nome, endereco);
     }
 }
