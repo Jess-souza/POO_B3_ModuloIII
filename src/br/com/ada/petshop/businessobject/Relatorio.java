3ba0438af9e178da8b2bcbde13945c0e5070d148
Jessica <jsouzasouza120@gmail.com>
2022-11-16 22:05:33 -0300

    Conceito e exemplos de Generics

--- src/br/com/ada/petshop/businessobject/Relatorio.java
+++ src/br/com/ada/petshop/businessobject/Relatorio.java
@@ -6,9 +6,7 @@ public class Relatorio<T> {
     public void geraRelatorioUnico(T t) {
         System.out.println("-----------------RELATORIO INDIVIDUAL--------------------");
         System.out.println(t);
-
     }
-
     public void geraRelatorioEmGrupo(List<T> lista) {
         System.out.println("-----------------RELATORIO COMPLETO--------------------");
         for (T t : lista) {
